cfb6eaade6006abbd5e4603d33855f15d77d0001
teckonn-seek <tmiew@seekasia.com>
2023-03-23 00:55:01 +0800

    try import db lib

--- apps/backoffice-api/src/main.ts
+++ apps/backoffice-api/src/main.ts
@@ -1,4 +1,5 @@
 import koa from 'koa';
+import { prisma } from '@sfa/backoffice-db';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -6,7 +7,8 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = new koa();
 
 app.use(async (ctx) => {
-  ctx.body = { message: 'Hello API' };
+  const count = await prisma.user.count();
+  ctx.body = { message: `User count ${count}` };
 });
 
 app.listen(port, host, () => {
