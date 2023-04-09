d028fe3237824ab8ccb79e9a8ebdcd9c68f55cb8
teckonn-seek <tmiew@seekasia.com>
2023-04-09 23:20:31 +0800

    format

--- apps/backoffice-api/src/main.ts
+++ apps/backoffice-api/src/main.ts
@@ -11,25 +11,28 @@ const App = new Koa();
 const router = new Router<any, Context>();
 
 const UserSchema = z.object({
-  id: z.bigint().nonnegative().transform(val => val.toString()),
+  id: z
+    .bigint()
+    .nonnegative()
+    .transform((val) => val.toString()),
   name: z.string().nullish(),
   email: z.string(),
-})
+});
 
 type UserSchema = z.infer<typeof UserSchema>;
 
 router.get('/users', async (ctx) => {
   try {
     const _users = await prisma.user.findMany();
-    ctx.body = _users.map(user => UserSchema.parse(user));;
+    ctx.body = _users.map((user) => UserSchema.parse(user));
     ctx.status = 200;
   } catch (error) {
     ctx.body = error;
     ctx.status = 500;
   }
-})
+});
 
-App.use(router.routes())
+App.use(router.routes());
 
 App.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
--- apps/backoffice-apps/app/users/page.tsx
+++ apps/backoffice-apps/app/users/page.tsx
@@ -1,3 +1,3 @@
 export default function User() {
-    return <h1 className="text-3xl font-bold">The User Page</h1>;
+  return <h1 className="text-3xl font-bold">The User Page</h1>;
 }
