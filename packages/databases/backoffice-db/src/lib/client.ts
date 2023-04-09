6c8b8211634f3a80967ea7c46f2d482abc66eb39
teckonn-seek <tmiew@seekasia.com>
2023-04-09 15:41:53 +0800

    chores: tweaks

--- packages/databases/backoffice-db/src/lib/client.ts
+++ packages/databases/backoffice-db/src/lib/client.ts
@@ -2,4 +2,19 @@ import { PrismaClient as BasePrismaClient } from '@prisma/client';
 
 class PrismaClient extends BasePrismaClient {}
 
-export const prisma = new PrismaClient();
+const prismaGlobal = global as typeof global & {
+  prisma?: PrismaClient;
+};
+
+export const prisma: PrismaClient =
+  prismaGlobal.prisma ||
+  new PrismaClient({
+    log:
+      process.env['NODE_ENV'] === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env['NODE_ENV'] !== 'production') {
+  prismaGlobal.prisma = prisma;
+}
